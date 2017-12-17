4f832071ae9fe2e1febc244da10090a939379e17
igorgad <igorgad@gmail.com>
2017-12-17 03:03:11 -0300

    network model is now implemented in pyRef.py

--- ITL.py
+++ ITL.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def gkernel(x, y, s):
-    return tf.divide(1.0,tf.sqrt(tf.multiply(tf.multiply(2.0,np.pi),s))) * tf.exp( tf.divide(tf.multiply(-1,tf.pow(tf.subtract(x,y), 2.0)),tf.multiply(2.0,tf.pow(s, 2.0))) )
+    return tf.divide(1.0,tf.sqrt(tf.multiply(tf.multiply(2.0,np.pi),s))) * tf.exp( tf.divide(tf.multiply(-1.0,tf.pow(tf.subtract(x,y), 2.0)),tf.multiply(2.0,tf.pow(s, 2.0))) )
 
 
 def ncc(x,y,marray,s):
@@ -14,11 +14,29 @@ def ncc(x,y,marray,s):
         def nloop(m):
             mm = tf.cast(m, tf.int32)
             N = tf.shape(x)[0]
-            nx = tf.range( start=tf.abs(tf.minimum(0,mm)), limit=tf.subtract(N, tf.abs(mm)) )
+            nx = tf.range( start=tf.abs(tf.minimum(0,mm)), limit=tf.subtract(N + 1, tf.abs(mm)) )
             ny = tf.add(nx, mm)
             return tf.reduce_mean(gkernel(tf.gather(x, nx), tf.gather(y, ny), s))
 
 
         return tf.map_fn(lambda m: nloop(m), marray)
 
+
+def ncclayer(ins,marray):
+    """ This function creates a computational graph for the correntropy layer.
+    ins is a tensor of shape [batchSize NumberOfSamples NumberOfWindows NumberOfSignals==2]
+    marray is a tensor with rank 1 containing m values to be analyzed """
+
+    [x,y] = tf.unstack(ins, axis=3)
+
+    N = tf.shape(ins)[1]
+    batchsize = tf.shape(ins)[0]
+    nwin = tf.shape(ins)[2]
+
+    Sigma = tf.Variable(1.0)
+
+    sx = tf.reshape(tf.transpose(x, [0, 2, 1]), [batchsize * nwin, N])
+    sy = tf.reshape(tf.transpose(y, [0, 2, 1]), [batchsize * nwin, N])
+
+    return tf.transpose(tf.reshape(tf.map_fn(lambda i: ncc(sx[i, :], sy[i, :], marray, Sigma), tf.range(batchsize * nwin), dtype=tf.float32), [batchsize, nwin, N]), [0, 2, 1])
 
--- pyRef.py
+++ pyRef.py
@@ -5,71 +5,128 @@ import numpy as np
 import ITL
 
 
+# TODO - encapsulate network params into a netparam dict
+##### NETWORK PARAMS #####
 
-def inference(images, hidden1_units, hidden2_units):
+marray = np.array(range(-80,80))
+medfiltersize = 8
+nsigs = 2
+nwin = 64
 
-    # Hidden 1
-    with tf.name_scope('hidden1'):
-        weights = tf.Variable(
-            tf.truncated_normal([IMAGE_PIXELS, hidden1_units],
-                                stddev=1.0 / math.sqrt(float(IMAGE_PIXELS))),
-            name='weights')
-        biases = tf.Variable(tf.zeros([hidden1_units]),
-                             name='biases')
-        hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
-    # Hidden 2
-    with tf.name_scope('hidden2'):
-        weights = tf.Variable(
-            tf.truncated_normal([hidden1_units, hidden2_units],
-                                stddev=1.0 / math.sqrt(float(hidden1_units))),
-            name='weights')
-        biases = tf.Variable(tf.zeros([hidden2_units]),
-                             name='biases')
-        hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
-    # Linear
-    with tf.name_scope('softmax_linear'):
-        weights = tf.Variable(
-            tf.truncated_normal([hidden2_units, NUM_CLASSES],
-                                stddev=1.0 / math.sqrt(float(hidden2_units))),
-            name='weights')
-        biases = tf.Variable(tf.zeros([NUM_CLASSES]),
-                             name='biases')
-        logits = tf.matmul(hidden2, weights) + biases
+medinit = 1/medfiltersize * np.ones((medfiltersize, 1, 1, nsigs), dtype=np.float32)
+
+shapeconv2 = [1, 9, 1, 128]
+shapeconv3 = [1, 5, 128, 64]
+shapeconv4 = [1, 5, 64, 32]
+
+fc1_nhidden = nwin * len(marray)
+fc2_nhidden = nwin * len(marray)
+nclass = len(marray)
+
+##########################
+
+
+def inference(ins, keep_prob):
+
+    # Conv 1 Layer (Mean Filter)
+    with tf.name_scope('conv_1'):
+        wc1 = tf.Variable( medinit )
+        bc1 =  tf.constant(0.0, shape=[nsigs])
+
+        conv1 = tf.nn.relu( tf.nn.conv2d(ins, wc1, strides=[1,1,1,1], padding='SAME') + bc1 )
+
+    # Normalized Cross Correntropy Layer
+    with tf.name_scope('ccc'):
+        ccc1 = ITL.ncclayer(conv1, marray)
+
+    # Conv 2 Layer
+    with tf.name_scope('conv_2'):
+        wc2 = tf.Variable( tf.truncated_normal(shape=shapeconv2, stddev=0.1) )
+        bc2 =  tf.constant(0.0, shape=[128])
+
+        conv2 = tf.nn.relu( tf.nn.conv2d(ccc1, wc2, strides=[1,1,1,1], padding='SAME') + bc2 )
+
+    with tf.name_scope('pool_2'):
+        pool2 = tf.nn.max_pool(conv2, ksize=[1, 1, 2, 1], strides=[1, 1, 2, 1], padding='SAME')
+
+    with tf.name_scope('dropout_conv2'):
+        drop2 = tf.nn.dropout(pool2, keep_prob)
+
+    # Conv 3 Layer
+    with tf.name_scope('conv_3'):
+        wc3 = tf.Variable( tf.truncated_normal(shape=shapeconv3, stddev=0.1) )
+        bc3 =  tf.constant(0.0, shape=[64])
+
+        conv3 = tf.nn.relu( tf.nn.conv2d(drop2, wc3, strides=[1,1,1,1], padding='SAME') + bc3 )
+
+    with tf.name_scope('pool_3'):
+        pool3 = tf.nn.max_pool(conv3, ksize=[1, 1, 2, 1], strides=[1, 1, 2, 1], padding='SAME')
+
+    with tf.name_scope('dropout_conv3'):
+        drop3 = tf.nn.dropout(pool3, keep_prob)
+
+    # Conv 4 Layer
+    with tf.name_scope('conv_4'):
+        wc4 = tf.Variable( tf.truncated_normal(shape=shapeconv4, stddev=0.1) )
+        bc4 =  tf.constant(0.0, shape=[32])
+
+        conv4 = tf.nn.relu( tf.nn.conv2d(drop3, wc4, strides=[1,1,1,1], padding='SAME') + bc4 )
+
+    with tf.name_scope('pool_4'):
+        pool4 = tf.nn.max_pool(conv4, ksize=[1, 1, 2, 1], strides=[1, 1, 2, 1], padding='SAME')
+
+    with tf.name_scope('dropout_conv4'):
+        drop4 = tf.nn.dropout(pool4, keep_prob)
+
+    #Flatten tensors
+    with tf.name_scope('flattening'):
+        flat4 = tf.reshape(drop4, [-1, tf.size(drop4)])
+
+    # FC 1 Layer
+    with tf.name_scope('fc_1'):
+        wfc1 = tf.Variable( tf.truncated_normal(shape=[tf.shape(flat4)[1],fc1_nhidden], stddev=0.1) )
+        bfc1 =  tf.constant(0.0, shape=[32])
+
+        fc1 = tf.nn.relu(tf.matmul(flat4, wfc1) + bfc1)
+
+    with tf.name_scope('dropout_fc1'):
+        dropfc1 = tf.nn.dropout(fc1, keep_prob)
+
+    # FC 2 Layer
+    with tf.name_scope('fc_2'):
+        wfc2 = tf.Variable( tf.truncated_normal(shape=[fc1_nhidden,fc2_nhidden], stddev=0.1) )
+        bfc2 =  tf.constant(0.0, shape=[32])
+
+        fc2 = tf.nn.relu(tf.matmul(dropfc1, wfc2) + bfc2)
+
+    with tf.name_scope('dropout_fc2'):
+        dropfc2 = tf.nn.dropout(fc2, keep_prob)
+
+    # Logits Layer
+    with tf.name_scope('logits'):
+        wl = tf.Variable(tf.truncated_normal(shape=[fc2_nhidden,nclass], stddev=0.1))
+        bl = tf.constant(0.0, shape=[32])
+
+        logits = tf.matmul(dropfc2, wl) + bl
     return logits
 
 
 def loss(logits, labels):
-    """Calculates the loss from the logits and the labels.
-    Args:
-      logits: Logits tensor, float - [batch_size, NUM_CLASSES].
-      labels: Labels tensor, int32 - [batch_size].
-    Returns:
-      loss: Loss tensor of type float.
-    """
     labels = tf.to_int64(labels)
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        labels=labels, logits=logits, name='xentropy')
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits, name='xentropy')
     return tf.reduce_mean(cross_entropy, name='xentropy_mean')
 
 
 def training(loss, learning_rate):
-    """Sets up the training Ops.
-    Creates a summarizer to track the loss over time in TensorBoard.
-    Creates an optimizer and applies the gradients to all trainable variables.
-    The Op returned by this function is what must be passed to the
-    `sess.run()` call to cause the model to train.
-    Args:
-      loss: Loss tensor, from loss().
-      learning_rate: The learning rate to use for gradient descent.
-    Returns:
-      train_op: The Op for training.
-    """
     # Add a scalar summary for the snapshot loss.
     tf.summary.scalar('loss', loss)
+
     # Create the gradient descent optimizer with the given learning rate.
     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+
     # Create a variable to track the global step.
     global_step = tf.Variable(0, name='global_step', trainable=False)
+
     # Use the optimizer to apply the gradients that minimize the loss
     # (and also increment the global step counter) as a single training step.
     train_op = optimizer.minimize(loss, global_step=global_step)
@@ -77,19 +134,6 @@ def training(loss, learning_rate):
 
 
 def evaluation(logits, labels):
-    """Evaluate the quality of the logits at predicting the label.
-    Args:
-      logits: Logits tensor, float - [batch_size, NUM_CLASSES].
-      labels: Labels tensor, int32 - [batch_size], with values in the
-        range [0, NUM_CLASSES).
-    Returns:
-      A scalar int32 tensor with the number of examples (out of batch_size)
-      that were predicted correctly.
-    """
-    # For a classifier model, we can use the in_top_k Op.
-    # It returns a bool tensor with shape [batch_size] that is true for
-    # the examples where the label is in the top k (here k=1)
-    # of all logits for that example.
     correct = tf.nn.in_top_k(logits, labels, 1)
     # Return the number of true entries.
     return tf.reduce_sum(tf.cast(correct, tf.int32))
