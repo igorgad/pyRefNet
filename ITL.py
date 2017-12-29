54fa512ce2c57cb9c64f9cd262244a1663c68374
igorgad <igorgad@gmail.com>
2017-12-29 17:43:22 -0300

    major perfomance improvment with real parallelized batches

--- ITL.py
+++ ITL.py
@@ -5,26 +5,6 @@ from pycuda.compiler import SourceModule
 
 import tensorflow as tf
 import numpy as np
-from numpy.lib.stride_tricks import as_strided
-
-
-def gkernel(x, y, s):
-    return tf.divide(1.0,tf.sqrt(tf.multiply(tf.multiply(2.0,np.pi),s))) * tf.exp( tf.divide(tf.multiply(-1.0,tf.pow(tf.subtract(x,y), 2.0)),tf.multiply(2.0,tf.pow(s, 2.0))) )
-
-
-def ncc(x,y,marray,s):
-    with tf.name_scope('ncc') as scope:
-
-        def nloop(m):
-            mm = tf.cast(m, tf.int32)
-            N = tf.shape(x)[0]
-            nx = tf.range( start=tf.abs(tf.minimum(0,mm)), limit=tf.subtract(N + 1, tf.abs(mm)) )
-            ny = tf.add(nx, mm)
-            return tf.reduce_mean(gkernel(tf.gather(x, nx), tf.gather(y, ny), s))
-
-
-        return tf.map_fn(lambda m: nloop(m), marray)
-
 
 def ncc_cuda(x,wx,y,wy,marray,s):
     """ x and y are 3d matrix. for every row pair of x and y, a normalized cross correntropy estimator is computed and stored in the nccr 3d matrix """
@@ -52,6 +32,28 @@ def ncc_cuda(x,wx,y,wy,marray,s):
     return nccr
 
 
+def gkernel(x, y, s):
+    return tf.divide(1.0,tf.sqrt(tf.multiply(tf.multiply(2.0,np.pi),s))) * tf.exp( tf.divide(tf.multiply(-1.0,tf.pow(tf.subtract(x,y), 2.0)),tf.multiply(2.0,tf.pow(s, 2.0))) )
+
+
+def ncc(x,y,marray,s):
+    with tf.name_scope('ncc') as scope:
+
+        def nloop(m):
+            N = tf.shape(x)[1]
+            nx = tf.range(start=tf.abs(tf.minimum(0, m)), limit=tf.subtract(N + 1, tf.abs(m)))
+            ny = tf.add(nx, m)
+
+            return tf.reduce_mean(gkernel(tf.gather(x,nx,axis=1), tf.gather(y,ny,axis=1), s), 1)
+
+        mb = tf.unstack(marray)
+        nccb = []
+        for m in mb:
+            nccb.append(nloop(m))
+
+        return tf.transpose(tf.stack(nccb), [1,0])
+
+
 def ncclayer(ins,marray):
     """ This function creates a computational graph for the correntropy layer.
     ins is a tensor of shape [batchSize NumberOfSamples NumberOfWindows NumberOfSignals==2]
@@ -59,16 +61,15 @@ def ncclayer(ins,marray):
 
     [x,y] = tf.unstack(ins, axis=3)
 
-    N = tf.shape(ins)[1]
     batchsize = tf.shape(ins)[0]
-    nwin = tf.shape(ins)[2]
+    nwin = tf.shape(ins)[1]
+    N = tf.shape(ins)[2]
 
     Sigma = tf.Variable(1.0)
 
     tf.summary.scalar('sigma', Sigma)
 
-    sx = tf.reshape(tf.transpose(x, [0, 2, 1]), [batchsize * nwin, N])
-    sy = tf.reshape(tf.transpose(y, [0, 2, 1]), [batchsize * nwin, N])
+    sx = tf.reshape(x, [batchsize * nwin, N])
+    sy = tf.reshape(y, [batchsize * nwin, N])
 
-    return tf.expand_dims(tf.transpose(tf.reshape(tf.map_fn(lambda i: ncc(sx[i, :], sy[i, :], marray, Sigma),
-                                                            tf.range(batchsize * nwin), dtype=tf.float32), [batchsize, nwin, marray.size]), [0, 2, 1]), 3)
+    return tf.expand_dims(tf.reshape(ncc(sx, sy, marray, Sigma), [batchsize, nwin, marray.size]), dim=3)
